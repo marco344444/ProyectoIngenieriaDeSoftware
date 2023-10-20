d15f34b693e134c9c4802befd9ac0f8d5697f5f5
Marco <pradamarco21@gmail.com>
2023-10-20 15:33:59 -0500

    acople de interfaz

--- src/Interfaz/Actividades_Empleado_venta.java
+++ src/Interfaz/Actividades_Empleado_venta.java
@@ -8,6 +8,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -16,9 +17,6 @@ public class Actividades_Empleado_venta extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -32,9 +30,6 @@ public class Actividades_Empleado_venta extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public Actividades_Empleado_venta() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 1300, 731);
@@ -43,37 +38,37 @@ public class Actividades_Empleado_venta extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JButton Atrás = new JButton("");
 		Atrás.setBorder(null);
 		Atrás.setIcon(new ImageIcon(Actividades_Empleado_venta.class.getResource("/paquetito/Botones/Atrás.jpg")));
-		Atrás.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
 		Atrás.setBounds(10, 24, 81, 23);
 		contentPane.add(Atrás);
-		
+
+		Atrás.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Redirige a la ventana de inicio de sesión en ventas
+				Inicio_Ventas newframe = new Inicio_Ventas();
+				newframe.setVisible(true);
+				dispose(); // Cierra la ventana actual
+			}
+		});
+
 		JButton Venta = new JButton("");
 		Venta.setBorder(null);
 		Venta.setIcon(new ImageIcon(Actividades_Empleado_venta.class.getResource("/paquetito/Botones/Venta.jpg")));
 		Venta.setBounds(84, 270, 505, 110);
 		contentPane.add(Venta);
-		
+
 		JButton Devoluciones = new JButton("");
 		Devoluciones.setBorder(null);
 		Devoluciones.setIcon(new ImageIcon(Actividades_Empleado_venta.class.getResource("/paquetito/Botones/Devoluciones.jpg")));
 		Devoluciones.setBounds(92, 408, 500, 110);
 		contentPane.add(Devoluciones);
-		
-		
+
 		JLabel Actividades_Venta = new JLabel("");
 		Actividades_Venta.setBounds(0, 0, 1300, 731);
 		Actividades_Venta.setIcon(new ImageIcon(Actividades_Empleado_venta.class.getResource("/paquetito/Imagenes/Actividades_Ventas.jpg")));
 		contentPane.add(Actividades_Venta);
-		
-		
-		
 	}
-
 }
