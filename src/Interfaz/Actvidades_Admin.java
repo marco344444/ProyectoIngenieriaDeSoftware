a210b0fc1994422c43fb189d6dea797593e35537
Marco <pradamarco21@gmail.com>
2023-10-23 00:29:07 -0500

    acoplada toda la interfaz de admin, falta corregir un error con la interfaz de mostrar inventario, pero de resto gud

--- src/Interfaz/Actvidades_Admin.java
+++ src/Interfaz/Actvidades_Admin.java
@@ -16,10 +16,9 @@ public class Actvidades_Admin extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JButton Empleados;
+	private JButton MirarInventario;
+	private JButton Proveedores;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -33,9 +32,6 @@ public class Actvidades_Admin extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public Actvidades_Admin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 1300, 731);
@@ -44,6 +40,7 @@ public class Actvidades_Admin extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+
 		JButton Atrás = new JButton("");
 		Atrás.setBorder(null);
 		Atrás.addActionListener(new ActionListener() {
@@ -51,50 +48,75 @@ public class Actvidades_Admin extends JFrame {
 				inicio newframe = new inicio();
 				newframe.setVisible(true);
 				dispose();
-				
 			}
 		});
 		Atrás.setIcon(new ImageIcon(Actvidades_Admin.class.getResource("/paquetito/Botones/Atrás.jpg")));
 		Atrás.setBounds(10, 24, 81, 23);
 		contentPane.add(Atrás);
-		
+
+		// Botón para abrir la ventana de Empleados
 		Empleados = new JButton("");
 		Empleados.setBorder(null);
 		Empleados.setIcon(new ImageIcon(Actvidades_Admin.class.getResource("/paquetito/Botones/Empleados.jpg")));
 		Empleados.setBounds(214, 221, 399, 65);
 		contentPane.add(Empleados);
-		
-		JButton Mirar_Inventario = new JButton("");
-		Mirar_Inventario.setBorder(null);
-		Mirar_Inventario.setIcon(new ImageIcon(Actvidades_Admin.class.getResource("/paquetito/Botones/Mirar_Inventario.jpg")));
-		Mirar_Inventario.setBounds(214, 346, 399, 57);
-		contentPane.add(Mirar_Inventario);
-		
-		JButton Proveedores = new JButton("");
+
+		Empleados.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Empleados newframe = new Empleados();
+				newframe.setVisible(true);
+				dispose();
+			}
+		});
+
+		// Botón para abrir la ventana de Mirar Inventario
+		MirarInventario = new JButton("");
+		MirarInventario.setBorder(null);
+		MirarInventario.setIcon(new ImageIcon(Actvidades_Admin.class.getResource("/paquetito/Botones/Mirar_Inventario.jpg")));
+		MirarInventario.setBounds(214, 346, 399, 57);
+		contentPane.add(MirarInventario);
+
+		MirarInventario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Mirar_Inventario newframe = new Mirar_Inventario();
+				newframe.setVisible(true);
+				dispose();
+			}
+		});
+
+		// Botón para abrir la ventana de Proveedores
+		Proveedores = new JButton("");
 		Proveedores.setBorder(null);
 		Proveedores.setIcon(new ImageIcon(Actvidades_Admin.class.getResource("/paquetito/Botones/Proveedores.jpg")));
 		Proveedores.setBounds(214, 463, 404, 57);
 		contentPane.add(Proveedores);
-		
-		JButton Generar_Reporte = new JButton("");
-		Generar_Reporte.setBorder(null);
-		Generar_Reporte.setIcon(new ImageIcon(Actvidades_Admin.class.getResource("/paquetito/Botones/Generar_Reporte.jpg")));
-		Generar_Reporte.setBounds(227, 590, 386, 55);
-		contentPane.add(Generar_Reporte);
-		
+
+		Proveedores.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Proveedores newframe = new Proveedores();
+				newframe.setVisible(true);
+				dispose();
+			}
+		});
+
+		// Botón para abrir la ventana de Generar Reporte
+		JButton GenerarReporte = new JButton("");
+		GenerarReporte.setBorder(null);
+		GenerarReporte.setIcon(new ImageIcon(Actvidades_Admin.class.getResource("/paquetito/Botones/Generar_Reporte.jpg")));
+		GenerarReporte.setBounds(227, 590, 386, 55);
+		contentPane.add(GenerarReporte);
+
+		GenerarReporte.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Generar_Reporte newframe = new Generar_Reporte();
+				newframe.setVisible(true);
+				dispose();
+			}
+		});
+
 		JLabel Actividades_Admin = new JLabel("");
 		Actividades_Admin.setBounds(0, 0, 1300, 731);
 		Actividades_Admin.setIcon(new ImageIcon(Actvidades_Admin.class.getResource("/paquetito/Imagenes/Actividades_Admin.jpg")));
 		contentPane.add(Actividades_Admin);
-		
-		
-		
-		
-		
-		
-		
-		
-		
 	}
-
 }
